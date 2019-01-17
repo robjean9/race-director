7a32eb983074ec9d73df5b91c6d8573133a98d9d
Jonathan Bursztyn <jonathan@bursztyn.io>
2019-01-17 15:11:34 -0300

    Improve webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,94 +1,60 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
+function getConfig(name, extension = "ts") {
+  return ({
+    entry: `./src/${name}/${name}.${extension}`,
+    target: `electron-${name}`,
+    output: {
+      filename: `${name}.bundle.js`,
+      path: __dirname + "/dist"
+    },
+    mode: "development",
+    node: {
+      __dirname: false,
+      __filename: false
+    },
+    resolve: {
+      extensions: [".js", ".json", ".ts", ".tsx"]
+    },
+    module: {
+      rules: [{
+          // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "ts-loader"
+          }
+        },
+        {
+          test: /\.(scss|css)$/,
+          use: ["style-loader", "css-loader?sourceMap", "sass-loader?sourceMap"]
+        },
+        {
+          test: /\.(jpg|png|svg|ico|icns)$/,
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[ext]"
+          }
+        },
+        {
+          test: /\.(eot|ttf|woff|woff2)$/,
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[ext]"
+          }
+        }
+      ]
+    },
+  })
+}
+
 let mainConfig = {
-  mode: "development",
-  entry: "./src/main/main.ts",
-  target: "electron-main",
-  output: {
-    filename: "main.bundle.js",
-    path: __dirname + "/dist"
-  },
-  node: {
-    __dirname: false,
-    __filename: false
-  },
-  resolve: {
-    extensions: [".js", ".json", ".ts"]
-  },
-  module: {
-    rules: [
-      {
-        // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-        test: /\.(ts)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader"
-        }
-      },
-      {
-        test: /\.(jpg|png|svg|ico|icns)$/,
-        loader: "file-loader",
-        options: {
-          name: "[path][name].[ext]"
-        }
-      },
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        loader: "file-loader",
-        options: {
-          name: "[path][name].[ext]"
-        }
-      }
-    ]
-  }
+  ...getConfig("main")
 };
 
 let rendererConfig = {
-  mode: "development",
-  entry: "./src/renderer/renderer.tsx",
-  target: "electron-renderer",
-  output: {
-    filename: "renderer.bundle.js",
-    path: __dirname + "/dist"
-  },
-  node: {
-    __dirname: false,
-    __filename: false
-  },
-  resolve: {
-    extensions: [".js", ".json", ".ts", ".tsx"]
-  },
-  module: {
-    rules: [
-      {
-        // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader"
-        }
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: ["style-loader", "css-loader?sourceMap", "sass-loader?sourceMap"]
-      },
-      {
-        test: /\.(jpg|png|svg|ico|icns)$/,
-        loader: "file-loader",
-        options: {
-          name: "[path][name].[ext]"
-        }
-      },
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        loader: "file-loader",
-        options: {
-          name: "[path][name].[ext]"
-        }
-      }
-    ]
-  },
+  ...getConfig("renderer", "tsx"),
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "./src/renderer/index.html")
